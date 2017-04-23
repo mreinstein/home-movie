9ef58064cc1ab18f021dfa350e26aedf53a206d9
Mike Reinstein <reinstein.mike@gmail.com>
2017-04-23 16:22:55 -0700

    simplified

--- service-video/public/bundle.js
+++ service-video/public/bundle.js
@@ -15,9 +15,7 @@ up.connect('ws://media-server:8002')
 
 async function getVideoList() {
   const response = await fetch('/videos')
-  const result = await response.json()
-  console.log('result:', result)
-  return result
+  return response.json()
 }
 
 async function renderVideoList() {
