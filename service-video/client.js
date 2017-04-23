0b229019642e19ee8d139f2200666c64f104c9c6
Mike Reinstein <reinstein.mike@gmail.com>
2017-04-23 16:10:02 -0700

    added casting capabilitiy

--- service-video/client.js
+++ service-video/client.js
@@ -23,12 +23,17 @@ async function renderVideoList() {
   const videos = await getVideoList()
   for(let i=0; i < videos.length; i++) {
     let a = document.createElement('a')
+    a.href = '#'
     a.innerText = videos[i]
     a.addEventListener('click', function(ev) {
       ev.preventDefault()
-      console.log('TODO: play::', videos[i])
+      console.log('playing', `http://media-server:8000/videos/${videos[i]}`)
+      up.send(JSON.stringify(JSON.stringify({ type: 'PLAY', mediaUrl: `http://media-server:8000/videos/${videos[i]}` })))
     })
-    document.body.appendChild(a)
+
+    let p = document.createElement('p')
+    p.appendChild(a)
+    document.body.appendChild(p)
   }
 }
 
